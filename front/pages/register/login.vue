fce75efc9b2649208bf209639d47f2b6acb4cc44
sabri-jammoussi <sabrijm123@gmail.com>
2024-02-26 16:50:20 +0100

    adding veelidate

--- front/pages/register/login.vue
+++ front/pages/register/login.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container class="mt-10" >
+  <v-container class="mt-10">
     <v-row align="center" justify="center" class="mt-10">
-      <v-col cols="12" sm="8" md="6" >
-        <v-card class="elevation-15" >
+      <v-col cols="12" sm="8" md="6">
+        <v-card class="elevation-15">
           <v-card-title class="headline grey lighten-2 text-center">
             <v-icon color="primary">mdi-account</v-icon>
             Sign-in!
@@ -11,16 +11,33 @@
           <!-- <Notification :message="error" v-if="error"/> -->
 
           <v-form @submit.prevent="login">
-            <v-text-field v-model="email" label="Email" type="email" required></v-text-field>
+            <v-alert type="error" v-if="errorListe" class="mt-2">
+              {{ errorListe }}
+            </v-alert>
+            <v-text-field
+              v-model="email"
+              label="Email"
+              type="email"
+              :rules="loginEmailRules"
+            ></v-text-field>
 
-            <v-text-field v-model="password" label="Password" type="password" required></v-text-field>
+            <v-text-field
+              v-model="password"
+              :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="show2 = !show2"
+              :type="show2 ? 'text' : 'password'"
+              label="Password"
+              :rules="[rules.required, rules.min]"
+            ></v-text-field>
 
             <v-btn color="black" block type="submit"> Login </v-btn>
           </v-form>
 
           <div class="text-center mt-5">
             Already got an account?
-            <nuxt-link to="/register/signup" class="primary--text">Sign-up</nuxt-link>
+            <nuxt-link to="/register/signup" class="primary--text"
+              >Sign-up</nuxt-link
+            >
           </div>
         </v-card>
       </v-col>
@@ -30,38 +47,44 @@
 
 <script setup>
 definePageMeta({
-  layout:"custom"
-})
-import { ref } from 'vue';
-import { useMyStore } from '@/store/index.js';
-import { useRouter } from 'vue-router';
+  layout: "custom",
+});
+import { ref } from "vue";
+import { useMyStore } from "@/store/index.js";
+import { useRouter } from "vue-router";
 
-const email = ref('');
-const password = ref('');
-
+const email = ref("");
+const password = ref("");
+const errorListe = computed(() => store.eroorlistee);
 const store = useMyStore();
 const router = useRouter();
+const show2 = ref(false);
 
+const rules = {
+  required: (value) => !!value || "Password obligatoire.",
+  min: (v) => (v && v.length >= 8) || "Min 8 caractères",
+};
+
+const loginEmailRules = [
+  (v) => !!v || "E-mail obligatoire",
+  (v) => /.+@.+\..+/.test(v) || "E-mail doit avoir @ et .Domain ",
+];
 const login = async () => {
-  if (!email.value || !password.value) {
-    alert('error');
-  } else {
-    const data = {
-      email: email.value,
-      password: password.value,
-    };
-    console.log('dataaaaaSended', data);
-      // Your login logic
-      await store.loginUser({ router }, data);
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log("dataaaaaSended", data);
+  // Your login logic
+  await store.loginUser({ router }, data);
 
-      // Check the status or response in the store, then navigate based on that.
-      // if (store.tokenLoaded) {
-      //   router.push('/');
-      // } else {
-      //   // Handle login failure, display an error message, etc.
-      //   alert('Login failed. Please check your credentials.');
-      // }
-    
-  }
+  //console.log('eroor  from login ',errorListe.value);
+  // Check the status or response in the store, then navigate based on that.
+  // if (store.tokenLoaded) {
+  //   router.push('/');
+  // } else {
+  //   // Handle login failure, display an error message, etc.
+  //   alert('Login failed. Please check your credentials.');
+  // }
 };
 </script>
--- front/pages/register/signup.vue
+++ front/pages/register/signup.vue
@@ -1,9 +1,9 @@
 <template>
-   <v-container class="mt-10">
+  <v-container class="mt-10">
     <v-row align="center" justify="center" class="mt-10">
       <v-col cols="12" sm="8" md="6">
         <v-card class="elevation-12">
-          <v-card-title class="headline grey lighten-2 text-center " > 
+          <v-card-title class="headline grey lighten-2 text-center">
             <v-icon color="primary">mdi-account-plus</v-icon>
             Register!
           </v-card-title>
@@ -11,17 +11,53 @@
           <!-- <Notification :message="error" v-if="error"/> -->
 
           <v-form @submit.prevent="signup">
-            <v-text-field v-model="firstName" label="firstName" required></v-text-field>
-            <v-text-field v-model="lastName" label="lastName" required></v-text-field>
-            <v-text-field v-model="email" label="Email" type="email" required></v-text-field>
-            <v-text-field v-model="password" label="Password" type="password" required></v-text-field>
-            <v-text-field v-model="role" label="role" required></v-text-field>
+            <v-text-field
+              v-model="firstName"
+              label="firstName"
+              @blur="v$.firstName.touch"
+              @input="v$.firstName.$touch"
+              :error-messages="v$.firstName.$errors.map((e) => e.$message)"
+            ></v-text-field>
+            <v-text-field
+              v-model="lastName"
+              label="lastName"
+              @blur="v$.lastName.touch"
+              @input="v$.lastName.$touch"
+              :error-messages="v$.lastName.$errors.map((e) => e.$message)"
+            ></v-text-field>
+            <v-text-field
+              v-model="email"
+              label="Email"
+              type="email"
+              @blur="v$.email.touch"
+              @input="v$.email.$touch"
+              :error-messages="v$.email.$errors.map((e) => e.$message)"
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              @blur="v$.password.touch"
+              @input="v$.password.$touch"
+              :error-messages="v$.password.$errors.map((e) => e.$message)"
+              :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="show2 = !show2"
+              :type="show2 ? 'text' : 'password'"
+            ></v-text-field>
+            <v-text-field
+              v-model="role"
+              label="role"
+              @blur="v$.role.touch"
+              @input="v$.role.$touch"
+              :error-messages="v$.role.$errors.map((e) => e.$message)"
+            ></v-text-field>
             <v-btn type="submit" color="black" block> Register </v-btn>
           </v-form>
 
           <div class="text-center mt-4">
             Already got an account?
-            <nuxt-link to="/register/login" class="primary--text">Login</nuxt-link>
+            <nuxt-link to="/register/login" class="primary--text"
+              >Login</nuxt-link
+            >
           </div>
         </v-card>
       </v-col>
@@ -30,27 +66,79 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { useMyStore } from '@/store/index.js';
-import { useRouter } from 'vue-router';
-
+import { ref } from "vue";
+import { useMyStore } from "@/store/index.js";
+import { useRouter } from "vue-router";
+import { useVuelidate } from "@vuelidate/core";
+import { required, helpers, minLength } from "@vuelidate/validators";
+const { withMessage, withAsync } = helpers;
+const show2 = ref(false);
 definePageMeta({
-  layout:"custom"
-})
+  layout: "custom",
+});
 
-const firstName = ref('');
-const lastName = ref('');
-const email = ref('');
-const password = ref('');
-const role = ref('');
+const firstName = ref("");
+const lastName = ref("");
+const email = ref("");
+const password = ref("");
+const role = ref("");
+
 const router = useRouter();
 
 const store = useMyStore();
 
+const rules = {
+  firstName: {
+    required: withMessage("First Name obligatoire", required),
+    min: withMessage("Min 3 caractères", minLength(3)),
+  },
+  lastName: {
+    required: withMessage("Last Name obligatoire", required),
+    min: withMessage("Min 3 caractères", minLength(3)),
+  },
+  email: {
+    required: withMessage("E-mail obligatoire", required),
+    emailRule: withMessage("email doit avoir  @ and .domain", (v) =>
+      /.+@.+\..+/.test(v)
+    ),
+  },
+  password: {
+    required: withMessage("Password obligatoire", required),
+    min: withMessage("Min 8 caractères", minLength(8)),
+    uppercase: withMessage("Au moins une lettre majuscule", (v) =>
+      /[A-Z]/.test(v)
+    ),
+    lowercase: withMessage("Au moins une lettre minuscule", (v) =>
+      /[a-z]/.test(v)
+    ),
+    Number : withMessage("Au moins un chiffre", (v)=>
+    /[0-9]/.test(v)
+    ),
+  },
+  role: {
+    required: withMessage("role obligatoire", required),
+  },
+};
+
+const v$ = useVuelidate(rules, {
+  firstName,
+  lastName,
+  email,
+  password,
+  role,
+});
 const signup = async () => {
-  if (!firstName.value || !email.value || !password.value) {
-    alert('Please Fill the Field');
+  // Clear previous validation errors
+  v$.value.$reset();
+
+  // Validate the form
+  v$.value.$touch();
+
+  // Check if there are any validation errors
+  if (v$.value.$pending || v$.value.$error) {
+    // Display an alert if there are validation errors
   } else {
+    // Form is valid, proceed with signup
     const data = {
       firstName: firstName.value,
       lastName: lastName.value,
@@ -58,11 +146,9 @@ const signup = async () => {
       password: password.value,
       role: role.value,
     };
-    console.log('dataaaaaSended', data);
+    console.log("dataaaaaSended", data);
 
-    await store.createUser({ router}, data );
+    await store.createUser({ router }, data);
   }
 };
-
-
 </script>
