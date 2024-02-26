817b418903f0ff092a6d9e427c7345c4327f9e7b
sabri-jammoussi <sabrijm123@gmail.com>
2024-02-26 16:51:07 +0100

    delete the fuction of Readuser and get error from the api

--- front/store/index.js
+++ front/store/index.js
@@ -6,6 +6,7 @@ export const useMyStore = defineStore('userStore', {
   state: () => ({
     token: '',
     user: null,
+    eroorlistee: null,
   }),
 
   getters: {
@@ -50,56 +51,82 @@ export const useMyStore = defineStore('userStore', {
         console.log('Response:', res);
 
         if (res.status >= 200 && res.status < 300) {
+          this.eroorlistee=null;
+
           // Now, you can safely use localStorage since this code is inside an action.
           localStorage.setItem('token', res.data.token);
+          if (res.data.token) {
+            const decodedToken = jwtDecode(res.data.token);
+            console.log("token decoded", decodedToken)
+            const userId = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+            const userFirstName = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"];
+            const userLastName = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname"];
+            const userEmail = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"];
+            const userRole = decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+            this.user = {
+              firstName: userFirstName,
+              lastName: userLastName,
+              email: userEmail,
+            };
+            console.log('user Data ', this.user);
+
+          }
           router.push('/');
+
         } else {
           alert(res.message);
         }
       } catch (error) {
         console.error('Error:', error);
+        if (error.response) {
+          if (error.response.status == 400) {
+            this.eroorlistee = error.response.data;
+            console.log('aaaaaaaaaaa', this.eroorlistee);
+          } 
+        }
+        
       }
     },
 
     async logoutUser({ router }) {
       localStorage.removeItem('token');
-      router.push("/register/login")
+      router.push("/register/login");
     },
     //task :  function to get the token then read the name and do it in the logout button !!  
-    async ReadUser() {
-      const token = window.localStorage.getItem('token');
+    // async ReadUser() {
+    //   const token = window.localStorage.getItem('token');
 
-      try {
-        if (token) {
-          // Decode the JWT to access its payload
-          const decodedToken = jwtDecode(token);
-          console.log('decodedToken',decodedToken);
-          const userId = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+    //   try {
+    //     if (token) {
+    //       // Decode the JWT to access its payload
+    //       const decodedToken = jwtDecode(token);
+    //       console.log('decodedToken',decodedToken);
+    //       const userId = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
 
-          console.log('User ID from token:', userId);
-          const response = await axios.get(`http://localhost:5252/api/Users/${userId}`, {
-            headers: {
-              'Authorization': `Bearer ${token}`,
-            },
-          });
-          if (response.status >= 200 && response.status < 300) {
-            // Update the user state directly or do something with the data
-            const userData= response.data
-            console.log('User data:', userData);
-            this.user=userData;
-            console.log('dataaaaaaaaaaa', this.user);
-              
-          } else {
-            alert('No data or invalid response status.');
-          }
-        } else {
-          alert('No token found.');
-        }
-      } catch (error) {
-        console.error('Error fetching user data:', error);
-        // Handle errors appropriately
-      }
-    },
+    //       console.log('User ID from token:', userId);
+    //       const response = await axios.get(`http://localhost:5252/api/Users/${userId}`, {
+    //         headers: {
+    //           'Authorization': `Bearer ${token}`,
+    //         },
+    //       });
+    //       if (response.status >= 200 && response.status < 300) {
+    //         // Update the user state directly or do something with the data
+    //         const userData= response.data
+    //         console.log('User data:', userData);
+    //         this.user=userData;
+    //         console.log('dataaaaaaaaaaa', this.user);
+
+    //       } else {
+    //         alert('No data or invalid response status.');
+    //       }
+    //     } else {
+    //       alert('No token found.');
+    //     }
+    //   } catch (error) {
+    //     console.error('Error fetching user data:', error);
+    //     // Handle errors appropriately
+    //   }
+    // },
 
     async CreateUser({ router }, data) {
       try {
@@ -109,6 +136,8 @@ export const useMyStore = defineStore('userStore', {
           // If there is a token, set the authorization header
           axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
           console.log('Token checked:', axios.defaults.headers.common);
+        } else {
+          console.log('unauthorized');
         }
 
         const response = await axios.post('http://localhost:5252/api/Users', data);
@@ -116,7 +145,7 @@ export const useMyStore = defineStore('userStore', {
         if (response.status >= 200 && response.status < 300) {
           router.push('/users/');
         } else {
-          alert(response.message);
+          alert('erorrooor', response.message);
         }
       } catch (error) {
         console.error('Error:', error);
