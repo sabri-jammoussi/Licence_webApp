ae19784b70be3b78473bc07024cc9fbf036071c4
sabri-jammoussi <sabrijm123@gmail.com>
2024-02-29 11:23:14 +0100

    delete the heriitage of NewUser to update all except the password

--- LicenceApp/LicenceApp/models/UpdateUser.cs
+++ LicenceApp/LicenceApp/models/UpdateUser.cs
@@ -2,9 +2,15 @@
 
 namespace LicenceApp.models
 {
-    public class UpdateUser : NewUser
+    public class UpdateUser 
     {
-   
+        public required string? FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public required string Email { get; set; }
+
+        public required Role Role { get; set; }
 
     }
 }
