06ab23c085aba5cd8418fe5896ec7ce59bc3bacd
sabri-jammoussi <sabrijm123@gmail.com>
2024-02-29 11:24:33 +0100

    adding the function of loading token from local Storage

--- front/store/index.js
+++ front/store/index.js
@@ -5,11 +5,11 @@ import { jwtDecode } from 'jwt-decode';
 export const useMyStore = defineStore('userStore', {
   state: () => ({
     token: '',
-    user: null,
+    user: '',
     eroorlistee: null,
     EmailError:null,
     roleOptions:null,
-    
+    userr:null,
   }),
 
   getters: {
@@ -77,8 +77,9 @@ export const useMyStore = defineStore('userStore', {
               firstName: userFirstName,
               lastName: userLastName,
               email: userEmail,
+              role: userRole,
             };
-            console.log('user Data ', this.user);
+            console.log('user Data from store', this.user);
 
           }
           router.push('/');
@@ -97,48 +98,39 @@ export const useMyStore = defineStore('userStore', {
         
       }
     },
+    async loadTokenFromLocalStorage(){
+      const token = window.localStorage.getItem('token');
+      try {
+            if(token){
+            // Decode the JWT to access its payload
+            const decodedToken = jwtDecode(token);
+            console.log('decodedToken  loadTokenFromLocalStorage',decodedToken);
+            const userId = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+            const userFirstName = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"];
+            const userLastName = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname"];
+            const userEmail = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"];
+            const userRole = decodedToken["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
+            this.user  = {
+              firstName: userFirstName,
+              lastName: userLastName,
+              email: userEmail,
+              role: userRole,
+            };
+            console.log('user Data from store loadTokenFromLocalStorage', this.user);
+          }
+      }catch (error) {
+      
+        }
+    },
 
     async logoutUser({ router }) {
       localStorage.removeItem('token');
       router.push("/register/login");
     },
-    //task :  function to get the token then read the name and do it in the logout button !!  
-    // async ReadUser() {
-    //   const token = window.localStorage.getItem('token');
 
-    //   try {
-    //     if (token) {
-    //       // Decode the JWT to access its payload
-    //       const decodedToken = jwtDecode(token);
-    //       console.log('decodedToken',decodedToken);
-    //       const userId = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+    async CreateUser(data) {
+      this.EmailError=null;
 
-    //       console.log('User ID from token:', userId);
-    //       const response = await axios.get(`http://localhost:5252/api/Users/${userId}`, {
-    //         headers: {
-    //           'Authorization': `Bearer ${token}`,
-    //         },
-    //       });
-    //       if (response.status >= 200 && response.status < 300) {
-    //         // Update the user state directly or do something with the data
-    //         const userData= response.data
-    //         console.log('User data:', userData);
-    //         this.user=userData;
-    //         console.log('dataaaaaaaaaaa', this.user);
-
-    //       } else {
-    //         alert('No data or invalid response status.');
-    //       }
-    //     } else {
-    //       alert('No token found.');
-    //     }
-    //   } catch (error) {
-    //     console.error('Error fetching user data:', error);
-    //     // Handle errors appropriately
-    //   }
-    // },
-
-    async CreateUser({ router }, data) {
       try {
         const token = window.localStorage.getItem('token');
 
@@ -148,17 +140,26 @@ export const useMyStore = defineStore('userStore', {
           console.log('Token checked:', axios.defaults.headers.common);
         } else {
           console.log('unauthorized');
+          alert('unauthorized');
         }
+       
 
         const response = await axios.post('http://localhost:5252/api/Users', data);
         console.log('dataRecivied', data);
         if (response.status >= 200 && response.status < 300) {
-          router.push('/users/');
+          // router.push('/users/');
+          this.EmailError=null;
         } else {
           alert('erorrooor', response.message);
         }
       } catch (error) {
         console.error('Error:', error);
+        if (error.response) {
+          if (error.response.status == 400) {
+            this.EmailError = error.response.data;
+            console.log('aaaaaaaaaaa', this.EmailError);
+          } 
+        }
       }
     }, 
 
