57dad9511afce5f023804216c5c047d14ec9e3a5
sabri-jammoussi <sabrijm123@gmail.com>
2024-03-04 16:57:38 +0100

    new templete with separeted code of crud

---
+++ front/pages/users/EditUser.vue
@@ -0,0 +1,163 @@
+<template>
+  <v-dialog v-model="editDialog" max-width="500px" >
+    <v-card>
+      <v-card-text>
+        <v-container>
+          <v-spacer></v-spacer>
+          <v-row>
+            <v-col cols="12" sm="6" md="12">
+              <v-text-field
+                v-model="firstName"
+                label="FirstName"
+                @blur="v$.firstName.$touch"
+                @input="v$.firstName.$touch"
+                :error-messages="v$.firstName.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="12">
+              <v-text-field
+                v-model="lastName"
+                label="LastName"
+                @blur="v$.lastName.$touch"
+                @input="v$.lastName.$touch"
+                :error-messages="v$.lastName.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="12">
+              <v-text-field
+                v-model="email"
+                label="Email"
+                @blur="v$.email.$touch"
+                @input="v$.email.$touch"
+                :error-messages="v$.email.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="12">
+              <v-select
+                v-model="role"
+                label="Role"
+                :items="roleOptions"
+                @blur="v$.role.$touch"
+                @input="v$.role.$touch"
+                :error-messages="v$.role.$errors.map((e) => e.$message)"
+              ></v-select>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="blue-darken-1"
+          variant="text"
+          @click="Updateuser"
+          :loading="loading"
+        >
+          Modifier
+        </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="close">
+          Annuler
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useMyStore } from "@/store/index.js";
+import { useVuelidate } from "@vuelidate/core";
+import { required, helpers, minLength } from "@vuelidate/validators";
+
+const { emit } = getCurrentInstance(); 
+const { withMessage } = helpers;
+const roleOptions = computed(() => store.roleOptions);
+const editDialog = ref(false);
+const store = useMyStore();
+const loading = ref(false);
+const firstName = ref("");
+const lastName = ref("");
+const email = ref("");
+const role = ref("");
+const id = ref("");
+const props = defineProps(['user']);
+
+const rules = {
+  firstName: {
+    required: withMessage("First Name obligatoire", required),
+    min: withMessage("Min 3 caractères", minLength(3)),
+  },
+  lastName: {
+    required: withMessage("Last Name obligatoire", required),
+    min: withMessage("Min 3 caractères", minLength(3)),
+  },
+  email: {
+    required: withMessage("E-mail obligatoire", required),
+    emailRule: withMessage("email doit avoir  @ and .domain", (v) =>
+      /.+@.+\..+/.test(v)
+    ),
+    InvalidEmail: withMessage("Email invaliddddd", (v) =>
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(v)
+    ),
+  },
+  role: {
+    required: withMessage("role obligatoire", required),
+  },
+};
+watch(editDialog, (val) => {
+  val || close();
+});
+
+const v$ = useVuelidate(rules, {
+  firstName,
+  lastName,
+  email,
+  role,
+},{$stopPropagation:true});
+
+const Updateuser = async () => {
+  loading.value = true;
+  v$.value.$touch();
+  if (v$.value.$pending || v$.value.$error) {
+    loading.value = false;
+  } else {
+    setTimeout(async () => {
+      try {
+        const userId = id.value;
+        console.log("idddddd", userId);
+        const data = {
+          firstName: firstName.value,
+          lastName: lastName.value,
+          email: email.value,
+          role: role.value,
+        };
+        await store.UpdateUser(userId, data);
+        console.log("UserSended", data);
+        loading.value = false;
+        await store.getUsers();
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        close();
+      }
+    }, 1500);
+  }
+};
+onMounted(async () => {
+    //console.log("edit dialog",props)
+    if (props.user) {
+      firstName.value = props.user.firstName;
+      lastName.value = props.user.lastName;
+      email.value = props.user.email;
+      role.value = props.user.role;
+      id.value = props.user.id;
+      editDialog.value = true;
+    }
+  await store.ReadRoles();
+});
+const close = () => {
+  v$.value.$reset();
+  editDialog.value = false;
+  emit('close-dialog');
+};
+</script>
---
+++ front/pages/users/UserList.vue
@@ -0,0 +1,115 @@
+<template>
+ 
+  <v-data-table
+    :headers="headers"
+    :items="users"
+    :sort-by="[{ key: 'calories', order: 'asc' }]"
+    v-model:search="search"
+    >
+   
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>
+          <v-text-field
+        v-model="search"
+        density="compact"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="solo-filled"
+        flat
+        hide-details
+        single-line
+        class="justify-content-start"
+      ></v-text-field>
+        </v-toolbar-title>
+      
+        <v-divider class=" mx-4" inset vertical></v-divider>
+   
+       
+        <AddUser />
+        <v-dialog v-model="dialogDelete" max-width="400">
+          <v-card>
+            <v-card-title>Delete Confirmation</v-card-title>
+            <v-card-text
+              >Voulez-vous vraiment supprimer cet utilisateur ?</v-card-text>
+            <v-card-actions>
+              <v-btn color="red" text @click="deleteItemConfirm"
+                >Supprimer</v-btn>
+              <v-btn color="primary" text @click="closeDelete">Annuler</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:[`item.actions`]="{ item }">
+      <v-icon size="small" class="me-2" @click="openEditDialog(item)">
+        mdi-pencil
+      </v-icon>
+      <v-icon size="small" @click.stop="deleteItem(item.id)">
+        mdi-delete
+      </v-icon>
+    </template>
+  </v-data-table>
+  <EditUser :user="selectedUser" v-if="editDialog" @close-dialog="editDialog = false" />
+</template>  
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import { useMyStore } from "@/store/index.js";
+import AddUser from "./AddUser.vue";
+import EditUser from "./EditUser.vue";
+
+const selectedUser = ref('');
+const store = useMyStore();
+const editDialog = ref(false);
+const dialogDelete = ref(false);
+const search = ref('');
+const headers = ref([
+  { title: "FirstName", key: "firstName" },
+  { title: "LastName", key: "lastName" },
+  { title: "Email", key: "email" },
+  { title: "Role", key: "role" },
+  { title: "Actions", key: "actions", sortable: false },
+]);
+const users = computed(() => store.users);
+const editedIndex = ref(-1);
+onMounted(async () => {
+  await store.getUsers();
+});
+const deleteItem = (utilisateurId) => {
+  editedIndex.value = utilisateurId;
+  dialogDelete.value = true;
+};
+const deleteItemConfirm = async () => {
+  const utilisateurId = editedIndex.value;
+  //console.log("iddddd", utilisateurId);
+  try {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      // If there is a token, set the authorization header
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      console.log("Token checked:", axios.defaults.headers.common);
+    } else {
+      console.log("unauthorized");
+      alert("unauthorized");
+    }
+    const res = await axios.delete(
+      `http://localhost:5252/api/Users/${utilisateurId}`
+    );
+    console.log("deleting user", res);
+    await store.getUsers();
+  } catch (err) {
+    console.error(err);
+  } finally {
+    closeDelete();
+  }
+};
+const openEditDialog = (item) => {
+  selectedUser.value = item;
+  editDialog.value = true;
+};
+const closeDelete = () => {
+  dialogDelete.value = false;
+};
+</script>
+    
