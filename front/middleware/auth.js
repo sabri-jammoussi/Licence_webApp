f6fae4a97b0a63ab87c7d6255afc39ff1d411dea
sabri-jammoussi <sabrijm123@gmail.com>
2024-03-05 14:08:27 +0100

    changed

--- front/middleware/auth.js
+++ front/middleware/auth.js
@@ -1,10 +1,12 @@
+
 export default defineNuxtRouteMiddleware((to, from) => {
     if (process.client) {
         const token = window.localStorage.getItem('token');
 
-        if (!token) {
-            return navigateTo('register/login')
+        if (!token && from.fullPath !=='/register/login') {
+            return navigateTo('/register/login')
         }
+        // return navigateTo('register/login')
     }
 
 });
