2264aa3b65080d7652da8479324a40f57f1cc6e3
sabri-jammoussi <sabrijm123@gmail.com>
2024-03-13 15:17:05 +0100

    adding footer

--- front/components/Footer.vue
+++ front/components/Footer.vue
@@ -1,5 +1,22 @@
 <template>
-      <span style="color: #16df17"
+<v-footer
+    style="
+      background-color: rgb(220, 220, 220);
+      color: #000;
+      caret-color: #000;
+      bottom: 0;
+      z-index: 1006;
+      transform: translateY(0%);
+      position: fixed;
+    "
+    app
+  >
+  <span style="color: #16df17"
           >&copy; APBS {{ new Date().getFullYear() }}</span
         >
+      </v-footer>
+
 </template>
+<style scoped>
+
+</style>
