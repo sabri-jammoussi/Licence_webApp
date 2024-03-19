e087a2ab7a889b399c45bbd831d4243dbd478557
sabri-jammoussi <sabrijm123@gmail.com>
2024-03-19 10:33:16 +0100

    adding the crud of client

---
+++ front/pages/Manager/Clients/EditClient.vue
@@ -0,0 +1,270 @@
+<template>
+  <v-dialog v-model="editDialog" max-width="500px">
+    <v-card>
+      <v-card-text>
+        <v-container>
+          <v-spacer></v-spacer>
+          <v-row>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                base-color="green"
+                :label="$t('Social reason')"
+                v-model="raisonSocial"
+                @blur="v$.raisonSocial.touch"
+                @input="v$.raisonSocial.$touch"
+                :error-messages="v$.raisonSocial.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                base-color="green"
+                :label="$t('identifier')"
+                v-model="identifiant"
+                @blur="v$.identifiant.touch"
+                @input="v$.identifiant.$touch"
+                :error-messages="v$.identifiant.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                base-color="green"
+                :label="$t('phone')"
+                type="number"
+                v-model="telephone"
+                @blur="v$.telephone.touch"
+                @input="v$.telephone.$touch"
+                :error-messages="v$.telephone.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                base-color="green"
+                label="Email"
+                v-model="email"
+                @blur="v$.email.touch"
+                @input="v$.email.$touch"
+                :error-messages="v$.email.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                base-color="green"
+                :label="$t('city')"
+                v-model="ville"
+                @blur="v$.ville.touch"
+                @input="v$.ville.$touch"
+                :error-messages="v$.ville.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                base-color="green"
+                :label="$t('address')"
+                v-model="adresse"
+                @blur="v$.adresse.touch"
+                @input="v$.adresse.$touch"
+                :error-messages="v$.adresse.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                v-model="codePostal"
+                :label="$t('Postal code')"
+                base-color="green"
+                @blur="v$.codePostal.touch"
+                @input="v$.codePostal.$touch"
+                :error-messages="v$.codePostal.$errors.map((e) => e.$message)"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-select
+                v-model="pays"
+                :label="$t('country')"
+                base-color="green"
+                :items="countryNames"
+                @blur="v$.pays.touch"
+                @input="v$.pays.$touch"
+                :error-messages="v$.pays.$errors.map((e) => e.$message)"
+              ></v-select>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-divider class="my-2"></v-divider>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="blue-darken-1"
+          variant="text"
+          @click="updateClient"
+          :loading="loading"
+        >
+          {{ $t("edit") }}
+        </v-btn>
+        <v-btn color="grey" variant="text" @click="close">
+          {{ $t("cancel") }}
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+    <script setup>
+import { ref, onMounted } from "vue";
+import { useMyStore } from "@/store/index.js";
+import { useVuelidate } from "@vuelidate/core";
+import { required, helpers, minLength } from "@vuelidate/validators";
+import axios from "axios";
+
+const { emit } = getCurrentInstance();
+const { withMessage } = helpers;
+const editDialog = ref(false);
+const store = useMyStore();
+const loading = ref(false);
+const identifiant = ref("");
+const raisonSocial = ref("");
+const telephone = ref("");
+const email = ref("");
+const ville = ref("");
+const adresse = ref("");
+const codePostal = ref("");
+const pays = ref("");
+const countryNames = ref([]);
+
+const id = ref("");
+const props = defineProps(["user"]);
+
+const rules = {
+  raisonSocial: {
+    required: withMessage("Identifiant obligatoire", required),
+  },
+  identifiant: {},
+  telephone: {
+    required: withMessage("Identifiant obligatoire", required),
+  },
+  email: {
+    required: withMessage("E-mail obligatoire", required),
+    emailRule: withMessage("email doit avoir  @ and .domain", (v) =>
+      /.+@.+\..+/.test(v)
+    ),
+    InvalidEmail: withMessage("Email invalid", (v) =>
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(v)
+    ),
+  },
+  ville: {
+    required: withMessage("Identifiant obligatoire", required),
+  },
+  adresse: {
+    required: withMessage("Identifiant obligatoire", required),
+  },
+  codePostal: {
+    required: withMessage("Identifiant obligatoire", required),
+  },
+  pays: {
+    required: withMessage("Identifiant obligatoire", required),
+  },
+};
+const v$ = useVuelidate(
+  rules,
+  {
+    identifiant,
+    raisonSocial,
+    telephone,
+    ville,
+    codePostal,
+    adresse,
+    pays,
+    email,
+  },
+  { $stopPropagation: true }
+);
+watch(editDialog, (val) => {
+  val || close();
+});
+
+const getCountries = async () => {
+  try {
+    const res = await axios.get(
+      "https://countriesnow.space/api/v0.1/countries/info?returns=name"
+    );
+    // Extract country names from the response and update countryNames
+    countryNames.value = res.data.data.map((country) => country.name);
+    console.log("Countries data:", countryNames.value);
+  } catch (error) {
+    console.error("Error fetching countries data:", error);
+  }
+};
+const updateClient = async () => {
+  loading.value = true;
+  v$.value.$touch();
+  if (v$.value.$pending || v$.value.$error) {
+    loading.value = false;
+  } else {
+    setTimeout(async () => {
+      try {
+        const userId = id.value;
+        console.log("idddddd", userId);
+        const data = {
+          id: userId,
+          raisonSocial: raisonSocial.value,
+          identifiant: identifiant.value,
+          telephone: telephone.value,
+          email: email.value,
+          ville: ville.value,
+          adresse: adresse.value,
+          codePostal: codePostal.value,
+          pays: pays.value,
+        };
+        const token = window.localStorage.getItem("token");
+        if (token) {
+          // If there is a token, set the authorization header
+          axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+          //console.log('Token checked:', axios.defaults.headers.common);
+        } else {
+          console.log("unauthorized");
+          alert("unauthorized");
+        }
+        const response = await axios.post(
+          `http://localhost:5252/api/client/modifierclient`,
+          data
+        );
+        if (response.status >= 200 && response.status < 300) {
+          // router.push('/users/');
+        } else {
+          alert("erorrooor", response.message);
+        }
+        loading.value = false;
+        await store.getClients();
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        close();
+      }
+    }, 1500);
+  }
+};
+onMounted(async () => {
+  //console.log("edit dialog",props)
+  getCountries();
+
+  if (props.user) {
+    raisonSocial.value = props.user.raisonSocial;
+    identifiant.value = props.user.identifiant;
+    (telephone.value = props.user.telephone),
+      (email.value = props.user.email),
+      (ville.value = props.user.ville),
+      (adresse.value = props.user.adresse),
+      (codePostal.value = props.user.codePostal),
+      (pays.value = props.user.pays),
+      (id.value = props.user.id);
+    editDialog.value = true;
+    console.log("props.user.id", props.user.id);
+  }
+  await store.ReadRoles();
+});
+const close = () => {
+  v$.value.$reset();
+  editDialog.value = false;
+  emit("close-dialog");
+};
+</script>
---
+++ front/pages/Manager/Clients/[id].vue
@@ -0,0 +1,111 @@
+<template>
+  <v-card>
+    <v-card-text>
+      <v-container>
+        <v-spacer></v-spacer>
+        <v-row no-gutters>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field
+              base-color="green"
+              :label="$t('raisonSocial')"
+              :disabled="true"
+              v-model="raisonSocial"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field
+              base-color="green"
+              :label="$t('identifier')"
+              :disabled="true"
+              v-model="identifiant"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field
+              base-color="green"
+              :label="$t('telephone')"
+              :disabled="true"
+              v-model="telephone"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field
+              base-color="green"
+              :label="$t('email')"
+              :disabled="true"
+              v-model="email"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field
+              base-color="green"
+              :label="$t('ville')"
+              :disabled="true"
+              v-model="ville"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field
+              base-color="green"
+              :label="$t('adresse')"
+              :disabled="true"
+              v-model="adresse"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field
+              v-model="codePostal"
+              :disabled="true"
+              :label="$t('codePostal')"
+              base-color="green"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="12">
+            <v-text-field
+              v-model="pays"
+              :disabled="true"
+              label="pays"
+              base-color="green"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+  
+  <script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const identifiant = ref("");
+const adresse = ref("");
+const raisonSocial = ref("");
+const pays = ref("");
+const ville = ref("");
+const email = ref("");
+const telephone = ref("");
+const codePostal = ref("");
+
+const route = useRoute();
+onMounted(async () => {
+  const id = route.params.id;
+  getClientById(id);
+  //console.log("iddddddddd", id);
+});
+const getClientById = (id) => {
+  axios.get(`http://localhost:5252/api/client/${id}`).then((res) => {
+    identifiant.value = res.data.identifiant;
+    adresse.value = res.data.adresse;
+    raisonSocial.value = res.data.raisonSocial;
+    ville.value = res.data.ville;
+    pays.value = res.data.pays;
+    email.value = res.data.email;
+    codePostal.value = res.data.codePostal;
+    telephone.value = res.data.telephone;
+  });
+};
+</script>
+  
+  <style>
+</style>
