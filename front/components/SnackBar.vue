4eb7019fec13eb2d4508d6a9c611c9edf5b5c642
sabri-jammoussi <sabrijm123@gmail.com>
2024-04-01 11:50:21 +0100

    adding sncakBar

---
+++ front/components/SnackBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-snackbar
+    v-model="snackbar"
+    :timeout="timeout"
+    color="success"
+    :location="location"
+  >
+    {{ message }}
+    <v-progress-linear v-if="showSnackBar" indeterminate color="white" />
+  </v-snackbar>
+</template>
+    
+  <script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  message: { type: String, required: true },
+  showSnackBar: { type: Boolean, required: true },
+  timeout: { type: Number, default: 2500 },
+  location: { type: String, default: "top" },
+});
+const snackbar = ref();
+onMounted(() => {
+  console.log("show snack bar",  props.showSnackBar);
+  console.log("show snack bar Message",  props.message);
+  snackbar.value = props.showSnackBar;
+});
+</script>
+  
---
+++ front/components/SnackBarError.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-snackbar
+    v-model="snackbarError"
+    :timeout="timeout"
+    color="error"
+    :location="location"
+  >
+    {{ message }}
+  </v-snackbar>
+</template>
+      
+    <script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  message: { type: String, required: true },
+  showSnackbarError: { type: Boolean, required: true },
+  timeout: { type: Number, default: 2500 },
+  location: { type: String, default: "top" },
+});
+const snackbarError = ref(false);
+onMounted(() => {
+  console.log("show snack bar errror", props.showSnackbarError);
+  console.log("show snack bar Message", props.message);
+  snackbarError.value = props.showSnackbarError;
+});
+</script>
+    
