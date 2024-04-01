4b2cfca04c46b9ea583e762a22e7b306da2de8b1
sabri-jammoussi <sabrijm123@gmail.com>
2024-04-01 11:54:01 +0100

    making the the verification of token in this js

---
+++ front/plugins/axios-auth.js
@@ -0,0 +1,20 @@
+// plugins/axios-auth.js
+import axios from "axios";
+import ErrorNotFound from "~/components/ErrorNotFound.vue";
+
+export default function () {
+   
+    if (process.client) {
+        const token = window.localStorage.getItem('token');
+
+        if (token) {
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        } else {
+            console.log('unauthorized');
+            // You might want to redirect to the login page or handle unauthorized access here
+            //alert("unauthorized");
+            
+             navigateTo('/') // Directly access router instance from SSR context
+        }
+    }
+}
